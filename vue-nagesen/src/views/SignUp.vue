a35a850c90c24dc21b6626182275897cfbf752af
kyafuso <kazuya.yafuso.0825@gmail.com>
2021-12-27 19:13:23 +0900

    非同期処理をvuexのactionに移行

--- vue-nagesen/src/views/SignUp.vue
+++ vue-nagesen/src/views/SignUp.vue
@@ -4,15 +4,15 @@
     <form >
       <div>
         <label for="name">ユーザー名</label>
-        <input type="text" id="name" v-model="name" placeholder="userName">
+        <input type="text" id="name" v-model="user.name" placeholder="userName">
       </div>
       <div class="form-group">
         <label for="email">メールアドレス</label>
-        <input type="email" id="email" v-model="email" placeholder="E-mail">
+        <input type="email" id="email" v-model="user.email" placeholder="E-mail">
       </div>
       <div class="form-group">
         <label for="password">パスワード</label>
-        <input type="password" id="password" v-model="password" placeholder="Password">
+        <input type="password" id="password" v-model="user.password" placeholder="Password">
       </div>
       <button type="button" class="button is-primary" @click="registerUser">送信する</button>
     </form>
@@ -21,16 +21,16 @@
 
 <script>
 import Title from '@/components/Title.vue'
-import firebase from 'firebase/compat/app'
-import "firebase/compat/auth"
 
 export default {
   name: 'SignUp',
   data : function() {
     return{
-      name : '',
-      email : '',
-      password : ''
+      user : {
+        name : '',
+        email : '',
+        password : ''
+      }
     }
   },
   components: {
@@ -40,15 +40,7 @@ export default {
   },
   methods: {
     registerUser(){
-      console.log(firebase);
-      firebase.auth().createUserWithEmailAndPassword(this.email, this.password)
-        .then((userCredential) => {
-          console.log(userCredential.user);
-        })
-        .catch((error) => {
-          console.log(error.code);
-          console.log(error.message);
-        });
+      this.$store.dispatch('registerUser', this.user)
     }
   }
 }
